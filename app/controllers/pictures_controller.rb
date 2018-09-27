b4d8d440022d01bd5120482d2ea01cba4afd5f78
MD MAMUNUR RASHID <netmamun@yahoo.com>
2018-09-27 01:59:56 -0400

    adding new pictures to database

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -9,14 +9,28 @@ class PicturesController < ApplicationController
      @picture = Picture.find(params[:id])
    end
 
-   def new
-      @picture = Picture.new
-    end
 
-    def create
-    end
 
-    def create
-        render text: "Received POST request to '/pictures' with the data URL: #{params}"
+    def new
+        @picture = Picture.new
       end
-end
+
+      def create
+        @picture = Picture.new
+
+        @picture.title = params[:picture][:title]
+        @picture.artist = params[:picture][:artist]
+        @picture.url = params[:picture][:url]
+
+
+        if @picture.save
+          # if the picture gets saved, generate a get request to "/pictures" (the index)
+          redirect_to "/pictures"
+        else
+          # otherwise render new.html.erb
+          render :new
+        end
+      end
+
+
+    end
